a19020ca92ffa0357de97f39d257f385540b17a0
Akbar Iskanderov <iskanderovofficial@gmail.com>
2024-06-23 22:01:02 +0500

    fixed bugs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ["@mui/icons-material/Star"],
+    },
+  },
   plugins: [react()],
-})
+});
